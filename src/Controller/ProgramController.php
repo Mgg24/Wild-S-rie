624ec494181082b4d2da8232549e5270bb861e05
Mgg24 <mgonotey.git@gmail.com>
2023-05-23 01:00:26 +0200

    Routing

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -5,17 +5,26 @@
     use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
     use Symfony\Component\HttpFoundation\Response;
     use Symfony\Component\Routing\Annotation\Route;
-
+    #[Route('/program', name: 'program_')]
     class ProgramController extends AbstractController
     {
-        #[Route('/program/', name: 'program_index')]
+
+        #[Route('/', name: 'index')]
         public function index(): Response
         {
             return $this->render('program/index.html.twig', [
                     'website' => 'Wild Series',
             ]);
         }
-    }
-    {
+         #[Route('/{id}/', name: 'show', requirements: ['page' =>'\d+'], methods: ['GET'])]
 
+        public function show(int $id): Response
+         {
+
+             $programTitle = 'Programme ' . $id;
+
+             return $this->render('program/show.html.twig', [
+                 'programTitle' => $programTitle,
+             ]);
+         }
     }
